3fdc9d0e5ae4b34e38c46f548ae8122e4ee29a4c
visualstudio <visualstudio@230-PC23>
2019-02-07 14:27:04 -0500

    2:05pm commit

--- week05/Program.cs
+++ week05/Program.cs
@@ -10,6 +10,29 @@ namespace week05
     {
         static void Main(string[] args)
         {
+            var a = new testquestion();
+            a.playingwithforloops();
+        }
+    }
+    class testquestion
+    {
+        public int myfavvariable = 0;
+        public void playingwithforloops()
+        {
+            for (; mymethod();)
+            {
+                if (myfavvariable > 10)
+                {
+                    Console.WriteLine("I am outof  the for loop");
+                    break;
+                }
+                Console.WriteLine("oh no ihave to go through this stupid loop again");
+            }
+        }
+        public bool mymethod()
+        {
+            myfavvariable++;
+            return true;
         }
     }
 }
